e5389ce0972ccfac19789188d59c5abe8f9a8560
Eduardo Saatkamp <eduardosaatkamp@gmail.com>
2025-08-31 22:24:06 -0300

    Add getCliente accessor and update CartaoDebito

--- src/Conta.java
+++ src/Conta.java
@@ -41,16 +41,20 @@ public abstract class Conta implements IConta {
 		return numero;
 	}
 
-	public double getSaldo() {
-		return saldo;
-	}
+        public double getSaldo() {
+                return saldo;
+        }
 
-	protected void imprimirInfosComuns() {
-		System.out.println(String.format("Titular: %s", this.cliente.getNome()));
-	 System.out.println(String.format("Agencia: %04d-%d", this.agencia / 10, this.agencia % 10));
+        public Cliente getCliente() {
+                return cliente;
+        }
+
+        protected void imprimirInfosComuns() {
+                System.out.println(String.format("Titular: %s", this.cliente.getNome()));
+         System.out.println(String.format("Agencia: %04d-%d", this.agencia / 10, this.agencia % 10));
         System.out.println(String.format("Numero: %05d-%d", this.numero / 10, this.numero % 10));
-		System.out.println(String.format("Saldo: %.2f", this.saldo));
-	}
+                System.out.println(String.format("Saldo: %.2f", this.saldo));
+        }
 
 	public CartaoDebito getCartaoDebito() {
 		return cartaoDebito;
